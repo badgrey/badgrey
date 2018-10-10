98d91ff01cbd9adfdb4cc38113f1dba6d3487a44
Marko Lisonek <mlisonek98@gmail.com>
2018-10-09 22:51:10 -0400

    feat(added schema for blog comments and set up associations)

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,14 +1,21 @@
 const User = require('./user')
 const Artist = require('./artist')
 const Blog = require('./blog')
+const Comment = require('./comment')
 
 User.belongsToMany(Artist, {through: 'Saved'})
 Artist.belongsToMany(User, {through: 'Saved'})
 Blog.belongsTo(User)
 User.hasMany(Blog)
+Comment.belongsTo(Blog)
+Blog.hasMany(Comment)
+User.belongsToMany(Comment, {through: 'Likes'})
+User.belongsToMany(Comment, {through: 'Dislikes'})
+Comment.belongsTo(User)
 
 module.exports = {
   User,
   Artist,
-  Blog
+  Blog,
+  Comment
 }
