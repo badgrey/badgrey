473d2f279fc9eca6bb9db2924e03caaf4d3418da
Marko Lisonek <mlisonek98@gmail.com>
2019-02-11 19:12:39 -0500

    fixed all artists componenet and artist hover styling

--- client/components/AllArtists.js
+++ client/components/AllArtists.js
@@ -2,26 +2,18 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import '../../public/style.css'
+import LazyLoad from 'react-lazyload'
 import {fetchArtists, fetchSavedArtists, fetchBlogs} from '../store'
-const Infinite = require('react-infinite')
 
 
-//componnt for all artists page
+//component for all artists page
 export class AllArtists extends Component{
 
   constructor(props) {
     super(props)
     this.state = {
       savedCheck: true,
-      search: '',
-      currentTitle: '# - C',
-      numToC: true,
-      dToG: false,
-      hToK: false,
-      lToO: false,
-      pToS: false,
-      tToW: false,
-      xtoZ: false
+      search: ''
     }
     this.saved = this.saved.bind(this)
     this.handleSearch = this.handleSearch.bind(this)
@@ -54,42 +46,9 @@ export class AllArtists extends Component{
   }
 
   render() {
-    // let numToCArtists = []
-    // let dToGArtists = []
-    // let hToKArtists = []
-    // let lToOArtists = []
-    // let pToSArtists = []
-    // let tToWArtists = []
-    // let xToZArtists = []
-    // for (let i = 0; i < this.props.artists.length; i++) {
-    //   let current = this.props.artists[i]
-    //   if ('xyz'.includes(current.name[0].toLowerCase())) {
-    //     xToZArtists.push(current)
-    //   } else if ('tuvw'.includes(current.name[0].toLowerCase())) {
-    //     tToWArtists.push(current)
-    //   } else if ('pqrs'.includes(current.name[0].toLowerCase())) {
-    //     pToSArtists.push(current)
-    //   } else if ('lmno'.includes(current.name[0].toLowerCase())) {
-    //     lToOArtists.push(current)
-    //   } else if ('hijk'.includes(current.name[0].toLowerCase())) {
-    //     hToKArtists.push(current)
-    //   } else if ('defg'.includes(current.name[0].toLowerCase())) {
-    //     dToGArtists.push(current)
-    //   } else {
-    //     numToCArtists.push(current)
-    //   }
-    // }
-    // numToCArtists = numToCArtists.filter((artist) => artist.name.toLowerCase().startsWith(this.state.search.toLowerCase()))
-    // dToGArtists = dToGArtists.filter((artist) => artist.name.toLowerCase().startsWith(this.state.search.toLowerCase()))
-    // hToKArtists = hToKArtists.filter((artist) => artist.name.toLowerCase().startsWith(this.state.search.toLowerCase()))
-    // lToOArtists = lToOArtists.filter((artist) => artist.name.toLowerCase().startsWith(this.state.search.toLowerCase()))
-    // pToSArtists = pToSArtists.filter((artist) => artist.name.toLowerCase().startsWith(this.state.search.toLowerCase()))
-    // tToWArtists = tToWArtists.filter((artist) => artist.name.toLowerCase().startsWith(this.state.search.toLowerCase()))
-    // xToZArtists = xToZArtists.filter((artist) => artist.name.toLowerCase().startsWith(this.state.search.toLowerCase()))
     const artists = this.props.artists.filter((artist) => artist.name.toLowerCase().startsWith(this.state.search.toLowerCase()))
     return (
       this.props.artists.length === 0 ? null :
-      <Infinite containerHeight={650} elementHeight={100}>
       <div className="artistsDiv">
         <h1 className="title">All Artists</h1>
         <div className="artistSearch">
@@ -98,10 +57,8 @@ export class AllArtists extends Component{
             <input onChange={this.handleSearch} placeholder="Name" />
           </form>
         </div>
-        <h1>{this.state.currentTitle}</h1>
         <div className="state">
         {
-          // !this.state.numToC ? null :
           artists.map((artist) => (
             //mapping over every artist and returning picture with link to artists page
               <div key={artist.id}>
@@ -109,14 +66,15 @@ export class AllArtists extends Component{
                   <div className="artistName">
                     <div className="artistNameText">{artist.name}</div>
                   </div>
-                  <img src={artist.imageURL} />
+                  <LazyLoad height={200}>
+                    <img src={artist.imageURL} />
+                  </LazyLoad>
                 </Link>
               </div>
           ))
         }
         </div>
       </div>
-      </Infinite>
     )
   }
 }
