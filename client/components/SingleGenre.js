e0465cfd73e9f655993e6cd76081ac9088cdfc38
mlisonek98 <31414927+mlisonek98@users.noreply.github.com>
2019-02-16 16:52:27 -0500

    Revert "fixed search"

--- client/components/SingleGenre.js
+++ client/components/SingleGenre.js
@@ -44,12 +44,11 @@ export class SingleGenre extends Component{
   }
 
   render() {
-    const artists = this.props.genreArtists.filter((artist) => artist.name.toLowerCase().includes(this.state.search.toLowerCase()))
+    const artists = this.props.genreArtists.filter((artist) => artist.name.toLowerCase().startsWith(this.state.search.toLowerCase()))
     return (
       this.props.genreArtists.length === 0 ? null :
       <div className="artistsDiv">
         <h1 className="title">{this.props.genreArtists[0].genre} Artists</h1>
-        <h6 className="scrolltoload">Scroll To Load More</h6>
         <div className="artistSearch">
           <form>
             <label className="searchLabel">Search Artist</label>
