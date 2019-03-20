2850d118574fd65b7ca2ff601bab62393d375055
Marko Lisonek <mlisonek98@gmail.com>
2019-03-19 22:18:19 -0400

    switching branches

--- client/components/Submit.js
+++ client/components/Submit.js
@@ -1,28 +1,57 @@
-import React from 'react'
-import {connect} from 'react-redux'
-import '../../public/style.css'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import axios from 'axios';
+import '../../public/style.css';
 
 //for now static submit page with information how to reach us
-const Submit = function() {
-  return (
-    <div className="submitContainerDiv">
-      <div className="submitHeader">
-        <h1>Submit Your Content To Us Here</h1>
-      </div>
-      <div className="submitContainer">
-        <h4>If you would like to submit content to us, or contact us for any reason, send the following information to</h4>
-        <h3>badgreyfilms@gmail.com</h3>
-        <h5>Your Name</h5>
-        <h5>Where You're From</h5>
-        <h5>Links To Your Soundcloud and Music Videos</h5>
-        <h6>Thank you!</h6>
-      </div>
-    </div>
-  )
+class Submit extends Component {
+
+  submit = async (evt) => {
+    evt.preventDefault();
+    const info = {
+      name: evt.target.name.value,
+      city: evt.target.city.value,
+      soundcloud: evt.target.soundcloud.value,
+      youtube: evt.target.youtube.value
+    }
+    await axios.post('/api/submit', info)
+  }
+  render(){
+    return (
+      <div className="outerForm">
+          <form  className="form" onSubmit={this.submit} name={name}>
+            <h2>Want To Be Featured On Bad Grey></h2>
+            <h4>Fill Out The Information Below</h4>
+            <div>
+              <label className="loginSignupLabel">Your Name</label>
+              <input className="loginInput" name="name" type="text" />
+            </div>
+            <div>
+              <label className="loginSignupLabel" htmlFor="email">Your Home City</label>
+              <input className="loginInput" name="city" type="text" />
+            </div>
+            <div>
+              <label className="loginSignupLabel" htmlFor="password">Link To Your Soundcloud</label>
+              <input className="loginInput" name="soundcloud" type="password" />
+            </div>
+            <div>
+              <label className="loginSignupLabel" htmlFor="password">Link To Your Music Video</label>
+              <input className="loginInput" name="youtube" type="password" />
+            </div>
+            <div>
+              <button type="submit">Send</button>
+            </div>
+          </form>
+        </div>
+    );
+  }
 }
 
-const mapState = null
+const mapState = null;
 
-const mapDispatch = null
+const mapDispatch = null;
 
-export default connect(mapState, mapDispatch)(Submit)
+export default connect(
+  mapState,
+  mapDispatch
+)(Submit);
