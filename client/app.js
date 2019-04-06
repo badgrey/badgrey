51e40cc3ea0420258bf2e0491089b79346bf7b00
Marko Lisonek <mlisonek98@gmail.com>
2019-04-06 12:15:48 -0400

    added mobile navbar

--- client/app.js
+++ client/app.js
@@ -1,14 +1,24 @@
 import React from 'react'
 
-import {Navbar} from './components'
+import {Navbar, MobileNavbar} from './components'
 import Footer from './components/Footer'
 import Routes from './routes'
+
+let mobile = false
+if (window.matchMedia('(max-width: 600px)').matches) {
+  mobile = true
+}
 
 //main app with navbar seperate from routes
 const App = () => {
   return (
     <div>
+    {
+      mobile ?
+      <MobileNavbar />
+      :
       <Navbar />
+    }
       <Routes />
       <Footer />
     </div>
