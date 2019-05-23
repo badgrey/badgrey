2126a8c9d830e25ce9b9c5dd93d2e7a1297ce654
Marko Lisonek <mlisonek98@gmail.com>
2019-05-23 18:46:15 -0400

    switched the pics fam

--- client/components/OriginalContent.js
+++ client/components/OriginalContent.js
@@ -11,18 +11,17 @@ const OriginalContent = () => {
       <div className="ocSelectorOptions">
       <div>
       <Link className="ocOption" to="/originalcontent/MusicVideo">
-        <img src="https://s3.us-east-2.amazonaws.com/badgrey-other/hmni.png" />
+        <img src="https://s3.us-east-2.amazonaws.com/badgrey-other/musicvideos.png" />
       </Link>
       </div>
       <div>
         <Link className="ocOption" to="/originalcontent/HelloMyNameIs">
-          <img src="https://s3.us-east-2.amazonaws.com/badgrey-other/musicvideos.png" />
+          <img src="https://s3.us-east-2.amazonaws.com/badgrey-other/hmni.png" />
         </Link>
       </div>
       </div>
     </div>
   )
 }
-
 
 export default OriginalContent
