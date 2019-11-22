a155823b8f1e7d8264a95e2f124502d342beaf37
Marko Lisonek <mlisonek98@gmail.com>
2019-11-21 22:23:44 -0500

    starting design for volume

--- server/api/bricks.js
+++ server/api/bricks.js
@@ -3,27 +3,32 @@ const {BricksVolume, BricksChapter} = require('../db/models')
 const asyncHandler = require('express-async-handler')
 module.exports = router
 
-router.get('/', asyncHandler(async (req, res, next) => {
-  const artists = await BricksVolume.findAll()
+router.get('/volume', asyncHandler(async (req, res, next) => {
+  const volumes = await BricksVolume.findAll()
+  res.json(volumes)
+}))
+
+router.get('/volume/:id', asyncHandler(async (req, res, next) => {
+  const volume = await BricksVolume.findById(req.params.id, {
+    include: [
+      {model: BricksChapter},
+    ]})
+  res.json(volume)
+}))
+
+
+router.get('/chapter/:id', asyncHandler(async (req, res, next) => {
+  const artists = await BricksChapter.findById(req.params.id)
   res.json(artists)
 }))
 
-router.get('/:chapter', asyncHandler(async (req, res, next) => {
-  const artists = await BricksChapter.findAll({
-    where: {
-      name: req.params.chapter
-    }})
-  res.json(artists)
-}))
+// router.post('/volume', asyncHandler(async (req, res, next) => {
+//   await BricksVolume.create({title: 'test volume', description: 'test', coverImage: 'test'})
+//   // res.json(newVolume)
+// }))
 
-router.post('/volume', asyncHandler(async (req, res, next) => {
-  console.log('in backend')
-  await BricksVolume.create({title: 'test volume', description: 'test', coverImage: 'test'})
-  // res.json(newVolume)
-}))
-
-router.post('/chapter', asyncHandler(async (req, res, next) => {
-  const newChapter = await BricksChapter.create({title: 'test volume', description: 'test', coverImage: 'test', pages: ['yerr']})
-  await newChapter.setBricksVolume(1)
-  // res.json(newChapter)
-}))
+// router.post('/chapter', asyncHandler(async (req, res, next) => {
+//   const newChapter = await BricksChapter.create({title: 'test volume', description: 'test', coverImage: 'test', pages: ['yerr']})
+//   await newChapter.setBricksVolume(1)
+//   // res.json(newChapter)
+// }))
