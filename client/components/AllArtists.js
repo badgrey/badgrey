a56d6d8490be91771a0bc1e7fde481f36d563348
Marko Lisonek <mlisonek98@gmail.com>
2020-03-17 15:46:02 -0400

    allartists.js updated to SASS

--- client/components/AllArtists.js
+++ client/components/AllArtists.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import '../../public/style.scss';
+import '../../public/styles/index.scss';
 import LazyLoad from 'react-lazyload';
 import { fetchArtists, fetchSavedArtists, fetchBlogs } from '../store';
 
--- client/components/Footer.js
+++ client/components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../../public/style.scss';
+import '../../public/styles/index.scss';
 
 const Footer = () => {
   return (
